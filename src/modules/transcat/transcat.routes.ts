e127e05e79f092216c04cf4d3ca22787c141d08b
grant farnsworth <gzp2307@wmich.edu>
2018-08-12 16:19:38 -0400

    added basic code to transcat

--- src/modules/transcat/transcat.routes.ts
+++ src/modules/transcat/transcat.routes.ts
@@ -1,6 +1,5 @@
 import * as express from 'express';
 import * as passport from 'passport';
-import * as LocalStrategy from 'passport-local';
 
 import transcatController from './trancat.controller';
 
@@ -9,5 +8,7 @@ export default (app: express.Express): void => {
     app.use(passport.initialize());
     app.use(passport.session());
 
-    app.get('api/transcat/')
+    app.post('api/transcat/addCatigories/',transcatController.addCatigories);
+
+    app.get('api/transcat/getCatigories/:transid',transcatController.getCatigories);
 };
